d75d79b94eea338096804a0db6eb1741d601212c
EhsanEsmaily747 <ehsanesmaily747@gmail.com>
2023-05-11 21:05:35 +0430

    choices and other methods added

--- lib/story.dart
+++ lib/story.dart
@@ -3,5 +3,9 @@ class Story{
   String? choice1;
   String? choice2;
   
-  Story(this.storyTitle,this.choice1,this.choice2);
+  Story({required String choice1,choice2,storyTitle}){
+    this.storyTitle=storyTitle;
+    this.choice1=choice1;
+    this.choice2=choice2;
+  }
 }
